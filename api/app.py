a82fa99fe07aa41f64246b3f6c2462d6031ef6b1
Julius Gasson <jg2023@ic.ac.uk>
2023-10-30 15:22:57 +0000

    Added name

--- api/app.py
+++ api/app.py
@@ -12,7 +12,6 @@ def hello_world():
 @app.route("/submit", methods=["POST"])
 def submit():
     input_name = request.form.get("name")
-    #comment
     input_age = request.form.get("age")
     return render_template("hello.html", name=input_name, age=input_age)
 
@@ -20,6 +19,8 @@ def submit():
 def process_query(query):
     if (query == "dinosaurs"):
         return "Dinosaurs ruled the Earth 200 million years ago"
+    elif ("name" in query):
+        return "Fly Devs"
     return "Unknown"
 
 
