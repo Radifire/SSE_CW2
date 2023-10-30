7878eb18df2fb229dd83fb1173605aa85394659a
Jo <Joseph.Aghoghovbia23@imperial.ac.uk>
2023-10-30 16:53:58 +0000

    Changed all tests to output an int instead of string

--- api/app_test.py
+++ api/app_test.py
@@ -20,20 +20,20 @@ def test_team_name():
 def test_largest_number():
 
     assert process_query("Which of the following \
-    numbers is the largest: 1, 72, 61?") == 72
+    numbers is the largest: 1, 72, 61?") == "72"
 
 
 def test_addition():
 
-    assert process_query("What is 39 plus 52?") == 91
+    assert process_query("What is 39 plus 52?") == "91"
 
 
 def test_multiplication():
 
-    assert process_query("What is 45 multiplied by 5?") == 225
+    assert process_query("What is 45 multiplied by 5?") == "225"
 
 
 def test_find_square_and_cube():
 
     assert process_query("Which of the following numbers is both a square \
-     and a cube: 3819, 944, 1772, 125, 319, 4356, 64?") == 64
+     and a cube: 3819, 944, 1772, 125, 319, 4356, 64?") == "64"
