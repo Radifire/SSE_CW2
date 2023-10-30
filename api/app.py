288763170ac859d2ab6fa390c6562fcabdd6ae87
Julius Gasson <jg2023@ic.ac.uk>
2023-10-30 17:06:38 +0000

    no i

--- api/app.py
+++ api/app.py
@@ -46,7 +46,7 @@ def process_query(query):
             if round(cube_root) ** 3 == i:
                 if round(square_root) ** 2 == i:
                     return str(i)
-    elif "multiplied" in i:
+    elif "multiplied" in query:
         numbers = get_numbers(query)
         return str(numbers[0] * numbers[1])
     elif "plus" in query:
